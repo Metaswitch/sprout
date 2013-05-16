44ba804f2c33e0ec14e6062623c91361b4feac67
Rob Day <Robert.Day@metaswitch.com>
2013-05-16 11:55:48 +0000

    Using trusted port rather than hardcoded 5058

--- sprout/deregister.cpp
+++ sprout/deregister.cpp
@@ -80,12 +80,26 @@ void send_register_to_as(pjsip_rx_data* received_register, pjsip_tx_data* ok_res
   pjsip_method method;
   pjsip_method_set(&method, PJSIP_REGISTER_METHOD);
 
-  // TODO: use stack_data.trusted_port
-  pj_str_t scscf_uri = pj_str(strdup(std::string("<sip:" + PJUtils::pj_str_to_string(&stack_data.sprout_cluster_domain) + ":5058>").c_str()));
+  char trusted_port[6] = "";
+  snprintf(trusted_port, 6, "%d", stack_data.trusted_port);
+
+  pj_str_t scscf_uri = pj_str(strdup(std::string(
+          "<sip:" +
+          PJUtils::pj_str_to_string(&stack_data.sprout_cluster_domain) + 
+          ":" + std::string(trusted_port) + ">").c_str()));
   pj_str_t as_uri = pj_str(strdup(as->server_name.c_str()));
 
-  pjsip_endpt_create_request(stack_data.endpt, &method, &as_uri, &scscf_uri, &as_uri, &scscf_uri, NULL, 1, NULL, &tdata);
- 
+  pjsip_endpt_create_request(stack_data.endpt,
+                             &method,
+                             &as_uri,
+                             &scscf_uri,
+                             &as_uri,
+                             &scscf_uri,
+                             NULL,
+                             1,
+                             NULL,
+                             &tdata);
+
   // Expires header based on 200 OK response
   pj_str_t expires_pjstr = pj_str("Expires");
   pjsip_hdr* expires_hdr = (pjsip_hdr*)pjsip_generic_int_hdr_create(tdata->pool, &expires_pjstr, expires);
@@ -97,73 +111,81 @@ void send_register_to_as(pjsip_rx_data* received_register, pjsip_tx_data* ok_res
   // TODO: Set P-Charging-Function-Addresses header based on HSS values
 
   if (received_register && ok_response) {
-  // Body based on Filter Criteria
-  char buf[8196];
-  pj_str_t sip_type = pj_str("message");
-  pj_str_t sip_subtype = pj_str("sip");
-  pj_str_t xml_type = pj_str("application");
-  pj_str_t xml_subtype = pj_str("3gpp-ims+xml");
 
-  // Build up this multipart body incrementally, based on the ServiceInfo, IncludeRegisterRequest and IncludeRegisterResponse fields
-  pjsip_msg_body* final_body = pjsip_multipart_create(tdata->pool, NULL, NULL);
+    pj_str_t pani_pjstr = pj_str("P-Access-Network-Info");
+    pj_str_t pvni_pjstr = pj_str("P-Visited-Network-Id");
+    pjsip_hdr* original_pani_hdr = pjsip_msg_find_hdr_by_name(received_register->msg_info.msg, &pani_pjstr, NULL);
+    if (original_pani_hdr) {
 
-  // If we only have one, we don't waant a multipart MIME body - store the reference to each one here to use instead
-  pjsip_msg_body* possible_final_body;
-  int multipart_parts = 0;
 
-  if (!as->service_info.empty()) {
+    // Body based on Filter Criteria
+    char buf[8196];
+    pj_str_t sip_type = pj_str("message");
+    pj_str_t sip_subtype = pj_str("sip");
+    pj_str_t xml_type = pj_str("application");
+    pj_str_t xml_subtype = pj_str("3gpp-ims+xml");
+
+    // Build up this multipart body incrementally, based on the ServiceInfo, IncludeRegisterRequest and IncludeRegisterResponse fields
+    pjsip_msg_body* final_body = pjsip_multipart_create(tdata->pool, NULL, NULL);
+
+    // If we only have one, we don't waant a multipart MIME body - store the reference to each one here to use instead
+    pjsip_msg_body* possible_final_body;
+    int multipart_parts = 0;
+
+    if (!as->service_info.empty()) {
       pjsip_multipart_part* xml_part = pjsip_multipart_create_part(tdata->pool);
       std::string xml_str = "<ims-3gpp><service-info>"+as->service_info+"</service-info></ims-3gpp>";
       pj_str_t xml_pj_str = pj_str(strdup(xml_str.c_str()));
       xml_part->body = pjsip_msg_body_create(tdata->pool, &xml_type, &xml_subtype, &xml_pj_str),
-      possible_final_body = xml_part->body;
+        possible_final_body = xml_part->body;
       multipart_parts++;
       pjsip_multipart_add_part(tdata->pool,
-                               final_body,
-                               xml_part);
-  }
+          final_body,
+          xml_part);
+    }
 
-  if (as->include_register_request)
+    if (as->include_register_request)
     {
       pjsip_multipart_part* request_part = pjsip_multipart_create_part(tdata->pool);
       pjsip_msg_print(received_register->msg_info.msg, buf, 8196);
       pj_str_t request_str = pj_str(buf);
       request_part->body = pjsip_msg_body_create(tdata->pool, &sip_type, &sip_subtype, &request_str),
-      possible_final_body = request_part->body;
+        possible_final_body = request_part->body;
       multipart_parts++;
       pjsip_multipart_add_part(tdata->pool,
-                               final_body,
-                               request_part);
+          final_body,
+          request_part);
     };
 
-  if (as->include_register_response)
+    if (as->include_register_response)
     {
       pjsip_multipart_part* response_part = pjsip_multipart_create_part(tdata->pool);
       pjsip_msg_print(ok_response->msg, buf, 8196);
       pj_str_t response_str = pj_str(buf);
       response_part->body = pjsip_msg_body_create(tdata->pool, &sip_type, &sip_subtype, &response_str),
-      possible_final_body = response_part->body;
+        possible_final_body = response_part->body;
       multipart_parts++;
       pjsip_multipart_add_part(tdata->pool,
-                               final_body,
-                               response_part);
+          final_body,
+          response_part);
     };
 
-  if (multipart_parts == 0) {
-    final_body = NULL;
-  } else if (multipart_parts == 1) {
-    final_body = possible_final_body;
-  } else {
-    // Just use the multipart MIME body you've built up
-  };
+    if (multipart_parts == 0) {
+      final_body = NULL;
+    } else if (multipart_parts == 1) {
+      final_body = possible_final_body;
+    } else {
+      // Just use the multipart MIME body you've built up
+    };
 
-  tdata->msg->body = final_body;
+    tdata->msg->body = final_body;
 
   }
 
   char for_printing[8196];
   pjsip_msg_print(tdata->msg, for_printing, 8186);
   LOG_DEBUG("Outgoing REGISTER request to AS: %s\n", for_printing);
+  printf("Outgoing REGISTER request to AS: %s\n", for_printing);
 
   status = pjsip_tsx_create_uac(&mod_registrar, tdata, &tsx);
   status = pjsip_tsx_send_msg(tsx, tdata);
@@ -178,10 +200,10 @@ void expire_bindings(RegData::Store* store, const std::string aor, const std::st
   //We need the retry loop to handle the store's compare-and-swap.
   RegData::AoR* aor_data;
   do
-    {
-      aor_data = store->get_aor_data(aor);
-      aor_data->remove_binding(binding_id);
-    }
+  {
+    aor_data = store->get_aor_data(aor);
+    aor_data->remove_binding(binding_id);
+  }
   while (!store->set_aor_data(aor, aor_data));
 
   delete aor_data;
@@ -190,10 +212,10 @@ void expire_bindings(RegData::Store* store, const std::string aor, const std::st
 // LCOV_EXCL_START
 /* Factored out from code not covered by UTs.
    Could be covered in future by:
-   * Putting a binding in a store
-   * Calling this function
-   * Checking the binding is gone
-*/
+ * Putting a binding in a store
+ * Calling this function
+ * Checking the binding is gone
+ */
 void network_initiated_deregistration(RegData::Store* store, const std::string aor, const std::string binding_id)
 {
   expire_bindings(store, aor, binding_id);
