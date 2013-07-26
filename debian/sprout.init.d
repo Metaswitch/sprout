5121b7f86df0f90b74135e4b7d2146041078cea3
Mike Evans <mike@metaswitch.com>
2013-07-26 15:13:47 +0100

    Change sprout startup to use local Infinispan store

--- debian/sprout.init.d
+++ debian/sprout.init.d
@@ -85,10 +85,6 @@ get_settings()
         sas_server=0.0.0.0
         . /etc/clearwater/config
 
-        # Pull in the current cluster configuration (creating a default if it does not exist).
-        [ -f /etc/clearwater/cluster_settings ] || echo "memcached_servers=$public_hostname:11211" > /etc/clearwater/cluster_settings
-        . /etc/clearwater/cluster_settings
-
         # Set up defaults for user settings then pull in any overrides.
         # Sprout uses blocking look-up services, so must run multi-threaded.
         num_pjsip_threads=1
@@ -96,7 +92,7 @@ get_settings()
         log_level=2
         [ -r /etc/clearwater/user_settings ] && . /etc/clearwater/user_settings
         [ -z "$enum_suffix" ] || enum_suffix_arg="--enum-suffix $enum_suffix"
-        [ -z "$enum_file" ] || enum_file_arg="--enum-file $enum_file" 
+        [ -z "$enum_file" ] || enum_file_arg="--enum-file $enum_file"
 }
 
 #
@@ -119,7 +115,7 @@ do_start()
         # enable gdb to dump a parent sprout process's stack
         echo 0 > /proc/sys/kernel/yama/ptrace_scope
         get_settings
-        DAEMON_ARGS="--system $NAME@$public_hostname --domain $home_domain --localhost $public_hostname --sprout-domain $sprout_hostname --alias $sprout_hostname,$public_ip --trusted-port 5054 --realm $home_domain --memstore $memcached_servers --hss $hs_hostname --xdms $xdms_hostname --enum $enum_server $enum_suffix_arg $enum_file_arg --sas $sas_server --pjsip-threads $num_pjsip_threads --worker-threads $num_worker_threads -a $log_directory -F $log_directory -L $log_level"
+        DAEMON_ARGS="--system $NAME@$public_hostname --domain $home_domain --localhost $public_hostname --sprout-domain $sprout_hostname --alias $sprout_hostname,$public_ip --trusted-port 5054 --realm $home_domain --memstore 127.0.0.1 --hss $hs_hostname --xdms $xdms_hostname --enum $enum_server $enum_suffix_arg $enum_file_arg --sas $sas_server --pjsip-threads $num_pjsip_threads --worker-threads $num_worker_threads -a $log_directory -F $log_directory -L $log_level"
 
         start-stop-daemon --start --quiet --background --make-pidfile --pidfile $PIDFILE --exec $DAEMON --chuid $NAME --chdir $HOME -- $DAEMON_ARGS \
                 || return 2
