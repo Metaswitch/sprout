4dad090844814e7af34a7e3db2c09b63e89540d0
Matt Williams <matt.williams@metaswitch.com>
2013-08-02 14:30:53 +0000

    Code tidy-up

--- include/accumulator.h
+++ include/accumulator.h
@@ -40,93 +40,65 @@
 #include <atomic>
 #include <time.h>
 
+#include "statistic.h"
+
 /// @class Accumulator
 ///
-/// Accumulates samples
+/// Accumulates samples, calculating mean, variance and low- and high-water
+/// marks on them.
 class Accumulator
 {
 public:
+  /// Default accumulation period, in microseconds.
   static const uint_fast64_t DEFAULT_PERIOD_US = 5 * 1000 * 1000;
 
+  /// Constructor.
   inline Accumulator(uint_fast64_t period_us = DEFAULT_PERIOD_US) :
                      _target_period_us(period_us)
   {
     reset();
   }
 
-  void accumulate(unsigned long sample)
-  {
-    // Update the basic counters and samples.
-    _current._n++;
-    _current._sigma += sample;
-    _current._sigma_squared += sample * sample;
+  /// Accumulate a sample into our results.
+  void accumulate(unsigned long sample);
+  /// Refresh our calculations - called at the end of each period, or
+  /// optionally at other times to get an up-to-date result.
+  void refresh(bool force = false);
+  /// Resets the accumulator.
+  void reset();
 
-    // Update the low- and high-water marks.  In each case, we get the current
-    // value, decide whether a change is required and then atomically swap it
-    // if so, repeating if it was changed in the meantime.
-    uint_fast64_t lwm = _current._lwm.load();
-    while ((sample < lwm) &&
-           (!_current._lwm.compare_exchange_weak(lwm, sample)))
-    {
-      // Do nothing.
-    }
-    uint_fast64_t hwm = _current._hwm.load();
-    while ((sample > hwm) &&
-           (!_current._hwm.compare_exchange_weak(hwm, sample)))
-    {
-      // Do nothing.
-    }
-
-    refresh();
-  }
-
-  void refresh(bool force = false)
-  {
-    uint_fast64_t timestamp = _current._timestamp.load();
-    uint_fast64_t timestamp_now = get_timestamp();
-    if ((force ||
-         (timestamp_now >= timestamp + _target_period_us)) &&
-        (_current._timestamp.compare_exchange_weak(timestamp, timestamp_now)))
-    {
-      read(timestamp_now - timestamp);
-      refreshed();
-    }
-  }
-
-  void reset()
-  {
-    _current._timestamp.store(get_timestamp());
-    _current._n.store(0);
-    _current._sigma.store(0);
-    _current._sigma_squared.store(0);
-    _current._lwm.store(MAX_UINT_FAST64);
-    _current._hwm.store(0);
-    _last._n = 0;
-    _last._mean = 0;
-    _last._variance = 0;
-    _last._lwm = 0;
-    _last._hwm = 0;
-  }
-
+  /// Get number of results in last period.
   inline uint_fast64_t get_n()        { return _last._n; }
+  /// Get mean.
   inline uint_fast64_t get_mean()     { return _last._mean; }
+  /// Get variance.
   inline uint_fast64_t get_variance() { return _last._variance; }
+  /// Get low-water mark.
   inline uint_fast64_t get_lwm()      { return _last._lwm; }
+  /// Get high-water mark.
   inline uint_fast64_t get_hwm()      { return _last._hwm; }
 
+  /// Callback whenever the accumulated statistics are refreshed.  Default is
+  /// to do nothing.
   virtual void refreshed() {};
 
 private:
+  /// Maximum value of a uint_fast64_t (assuming 2s-complement).  There is a
+  /// #define for this, but it's unavailable in C++.
   static const uint_fast64_t MAX_UINT_FAST64 = ~((uint_fast64_t)0);
 
+  /// Target period (in microseconds) over which samples are accumulated.
+  /// Might be inaccurate due to timing errors, or because events don't come
+  /// in frequently enough.
   uint_fast64_t _target_period_us;
 
-  // We use a set of atomics here.  This isn't perfect, as reads are not
-  // synchronized (e.g. we could read a value of _n that is more recent than
-  // the value we read of _sigma).  However, given that _n is likely to be
-  // quite large and only out by 1 or 2, it's not expected to matter.
+  /// Set of current statistics being accumulated.
   struct {
-    std::atomic_uint_fast64_t _timestamp;
+    // We use a set of atomics here.  This isn't perfect, as reads are not
+    // synchronized (e.g. we could read a value of _n that is more recent than
+    // the value we read of _sigma).  However, given that _n is likely to be
+    // quite large and only out by 1 or 2, it's not expected to matter.
+    std::atomic_uint_fast64_t _timestamp_us;
     std::atomic_uint_fast64_t _n;
     std::atomic_uint_fast64_t _sigma;
     std::atomic_uint_fast64_t _sigma_squared;
@@ -134,6 +106,7 @@ private:
     std::atomic_uint_fast64_t _hwm;
   } _current;
 
+  /// Set of statistics accumulated over the previous period.
   struct {
     volatile uint_fast64_t _n;
     volatile uint_fast64_t _mean;
@@ -142,7 +115,8 @@ private:
     volatile uint_fast64_t _hwm;
   } _last;
 
-  inline uint_fast64_t get_timestamp()
+  /// Get a timestamp in microseconds.
+  inline uint_fast64_t get_timestamp_us()
   {
     uint_fast64_t timestamp = 0;
     struct timespec ts;
@@ -153,39 +127,29 @@ private:
     return timestamp;
   }
 
-  void read(uint_fast64_t period_us)
-  {
-    uint_fast64_t n = _current._n.exchange(0);
-    uint_fast64_t sigma = _current._sigma.exchange(0);
-    uint_fast64_t sigma_squared = _current._sigma_squared.exchange(0);
-    _last._n = n * period_us / _target_period_us;
-    uint_fast64_t mean = (n > 0) ? sigma / n : 0;
-    _last._mean = mean;
-    _last._variance = (n > 0) ? ((sigma_squared / n) - (mean * mean)) : 0;
-    _last._lwm = _current._lwm.exchange(MAX_UINT_FAST64);
-    _last._hwm = _current._hwm.exchange(0);
-  }
+  /// Read the accumulated statistics, calculate their properties and report
+  /// them as the last set of statistics.
+  void read(uint_fast64_t period_us);
 };
 
+/// @class StatisticAccumulator
+///
+/// Accumulates statistics and reports them as a zeroMQ-based statistic.
 class StatisticAccumulator : public Accumulator
 {
 public:
+  /// Constructor.
   inline StatisticAccumulator(std::string statname,
                               uint_fast64_t period_us = DEFAULT_PERIOD_US) :
                               Accumulator(period_us),
                               _statistic(statname) {}
 
-  virtual void refreshed()
-  {
-    std::vector<std::string> values;
-    values.push_back(std::to_string(get_mean()));
-    values.push_back(std::to_string(get_variance()));
-    values.push_back(std::to_string(get_lwm()));
-    values.push_back(std::to_string(get_hwm()));
-    _statistic.report_change(values);
-  }
+  /// Callback whenever the accumulated statistics are refreshed.  Passes
+  /// values to zeroMQ.
+  virtual void refreshed();
 
 private:
+  /// The zeroMQ-based statistic to report to.
   Statistic _statistic;
 };
 
