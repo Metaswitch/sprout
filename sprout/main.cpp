fe14ba848891b9777a276d1229bccaaba4d53c7d
Mike Evans <mike@metaswitch.com>
2013-09-02 09:21:06 +0100

    Sort memcached server list so all sprout nodes see the same list

--- sprout/main.cpp
+++ sprout/main.cpp
@@ -608,6 +608,7 @@ int main(int argc, char *argv[])
     LOG_STATUS("Using memcached compatible store with ASCII protocol");
     std::list<std::string> servers;
     Utils::split_string(opt.store_servers, ',', servers, 0, true);
+    servers.sort();
     registrar_store = RegData::create_memcached_store(servers, 100, false);
   }
   else
