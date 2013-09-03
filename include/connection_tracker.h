c827cfdc064c3cd9d70b8e9c1979ec8baec6fa0b
Alex Hockey <alex.hockey@metaswitch.com>
2013-09-03 12:23:47 +0100

    Implement the connection tracker

---
+++ include/connection_tracker.h
@@ -0,0 +1,103 @@
+/**
+ * @file connection_tracker.h Definition of COnnectionTracker - a class used to
+ * track and manage TCP connections.
+ *
+ * Project Clearwater - IMS in the Cloud
+ * Copyright (C) 2013  Metaswitch Networks Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version, along with the "Special Exception" for use of
+ * the program along with SSL, set forth below. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * The author can be reached by email at clearwater@metaswitch.com or by
+ * post at Metaswitch Networks Ltd, 100 Church St, Enfield EN2 6BQ, UK
+ *
+ * Special Exception
+ * Metaswitch Networks Ltd  grants you permission to copy, modify,
+ * propagate, and distribute a work formed by combining OpenSSL with The
+ * Software, or a work derivative of such a combination, even if such
+ * copying, modification, propagation, or distribution would otherwise
+ * violate the terms of the GPL. You must comply with the GPL in all
+ * respects for all of the code used other than OpenSSL.
+ * "OpenSSL" means OpenSSL toolkit software distributed by the OpenSSL
+ * Project and licensed under the OpenSSL Licenses, or a work based on such
+ * software and licensed under the OpenSSL Licenses.
+ * "OpenSSL Licenses" means the OpenSSL License and Original SSLeay License
+ * under which the OpenSSL Project distributes the OpenSSL toolkit software,
+ * as those licenses appear in the file LICENSE-OPENSSL.
+ */
+
+
+#ifndef CONNECTION_TRACKER_H__
+#define CONNECTION_TRACKER_H__
+
+extern "C" {
+#include <pjsip.h>
+#include <pjlib-util.h>
+#include <pjlib.h>
+}
+
+// Common STL includes.
+#include <cassert>
+#include <map>
+#include <unordered_map>
+#include <string>
+
+class ConnectionTracker
+{
+public:
+  ConnectionTracker();
+  ~ConnectionTracker();
+
+  // Notify the connection tracker that a connection is active (usually because
+  // a message has been received on it).
+  void connection_active(pjsip_transport *tp);
+
+  // Quiesce all connections.  When this is called all current connections are
+  // gracefully shutdown, and the connection tracker is put in a state where
+  // subsequent new connections are also gracefully shutdown.
+  //
+  // It is only legal to call this method when the connection tracker is
+  // in normal operation (there has never been a call to quiesce, or there
+  // hasn't been on since the most recent call to unquiesce).
+  void quiesce();
+
+  // Unquiesce.  This puts the connection tracker into normal operation (where
+  // new connections are not automatically shutdown).
+  //
+  // It is only legal to call this method when the connection tracker is
+  // quiescing (there has been a previous call to quiesce without an intervening
+  // call to unquiesce).
+  void unquiesce();
+
+  // Static method that is registered as a transport state listener so the
+  // connection tracker is told when a connection disconnects.
+  static void connection_state(pjsip_transport* tp,
+                               pjsip_transport_state state,
+                               const pjsip_transport_state_info* info);
+
+private:
+  // This must be held when accessing any of this object's member variables.
+  pthread_mutex_t _lock;
+
+  // A set of all the connections known to the connection manager.  This is a
+  // set of pjsip transports, but only includes connection-based transports (not
+  // datagram transports).
+  std::set<pjsip_transport *> _connections;
+
+  // Whether the connection manager is quiescing it's connections.
+  pj_bool_t _quiescing;
+
+  void connection_state_update(pjsip_transport* tp,
+                               pjsip_transport_state state);
+};
+
+#endif
---
+++ sprout/connection_tracker.cpp
@@ -0,0 +1,180 @@
+/**
+ * @file connection_tracker.cpp
+ *
+ * Project Clearwater - IMS in the Cloud
+ * Copyright (C) 2013  Metaswitch Networks Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version, along with the "Special Exception" for use of
+ * the program along with SSL, set forth below. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * The author can be reached by email at clearwater@metaswitch.com or by
+ * post at Metaswitch Networks Ltd, 100 Church St, Enfield EN2 6BQ, UK
+ *
+ * Special Exception
+ * Metaswitch Networks Ltd  grants you permission to copy, modify,
+ * propagate, and distribute a work formed by combining OpenSSL with The
+ * Software, or a work derivative of such a combination, even if such
+ * copying, modification, propagation, or distribution would otherwise
+ * violate the terms of the GPL. You must comply with the GPL in all
+ * respects for all of the code used other than OpenSSL.
+ * "OpenSSL" means OpenSSL toolkit software distributed by the OpenSSL
+ * Project and licensed under the OpenSSL Licenses, or a work based on such
+ * software and licensed under the OpenSSL Licenses.
+ * "OpenSSL Licenses" means the OpenSSL License and Original SSLeay License
+ * under which the OpenSSL Project distributes the OpenSSL toolkit software,
+ * as those licenses appear in the file LICENSE-OPENSSL.
+ */
+
+#include "connection_tracker.h"
+
+ConnectionTracker::ConnectionTracker() :
+  _connections(),
+  _quiescing(PJ_FALSE)
+{
+  pthread_mutex_init(&_lock);
+}
+
+
+ConnectionTracker::~ConnectionTracker()
+{
+  pthread_mutex_destroy(&_lock);
+}
+
+
+void ConnectionTracker::connection_state(pjsip_transport *tp,
+                                         pjsip_transport_state state,
+                                         const pjsip_transport_state_info *info)
+{
+  ((ConnectionTracker *)info->user_data)->connection_state_update(tp, state);
+}
+
+
+void ConnectionTracker::connection_state_update(pjsip_transport *tp,
+                                                pjsip_transport_state state)
+{
+  pj_bool_t quiesce_complete = PJ_FALSE;
+
+  if (state == PJSIP_TP_STATE_DISCONNECTED)
+  {
+    LOG_DEBUG("Connection %p has disconnected", tp);
+
+    pthread_mutex_lock(&_lock);
+
+    _connections.erase(tp);
+
+    // If we're quiescing and there are no more active connections, then
+    // quiescing is complete.
+    if (_quiescing && _connections.empty()) {
+      LOG_DEBUG("Connection quiescing complete");
+      quiesce_complete = PJ_FALSE;
+    }
+
+    pthread_mutex_unlock(&_lock);
+
+    // If quiescing is now complete notify the quiescing manager.  This is done
+    // without holding the lock to avoid potential deadlocks.
+    if (quiesce_complete) {
+      // TODO Call back into the quiescing manager.
+    }
+  }
+}
+
+
+void ConnectionTracker::connection_active(pjsip_transport *tp)
+{
+  // We only track connection-oriented transports.
+  if ((tp->flag & PJSIP_TRANSPORT_DATAGRAM) == 0)
+  {
+    pthread_mutex_lock(&_lock);
+
+    if (_connections.find(tp) != set::end)
+    {
+      // New connection. Add it to the connections set, and register a state
+      // listener so we know when it gets destroyed.
+      _connections.insert(tp);
+
+      pjsip_tp_state_listener_key *key;
+      pjsip_transport_add_state_listener(tp,
+                                         &connection_state,
+                                         (void *)this,
+                                         &key);
+
+      // If we're quiescing, shutdown the transport immediately.  The connection
+      // will be closed when all transactions that use it have ended.
+      if (_quiescing) {
+        pjsip_transport_shutdown(tp);
+      }
+    }
+
+    pthread_mutex_unlock(&_lock);
+  }
+}
+
+
+void ConnectionTracker::quiesce()
+{
+  pj_bool_t quiesce_complete = PJ_FALSE;
+
+  LOG_DEBUG("Start quiescing connections");
+
+  pthread_mutex_lock(&_lock);
+
+  // Flag that we're now quiescing. It is illegal to call this method if we're
+  // already quiescing.
+  assert(!_quiescing);
+  _quiescing = PJ_TRUE;
+
+  if (_connections.empty())
+  {
+    // There are no active connections, so quiescing is already complete.
+    LOG_DEBUG("Connection quiescing complete");
+    quiesce_complete = PJ_TRUE;
+  }
+  else
+  {
+    // Call shutdown on each connection. PJSIP's reference counting means a
+    // connection will be closed once all transactions that use it have
+    // completed.
+    for (auto it = _connections.begin(); it != _connections.end(); ++it)
+    {
+      LOG_DEBUG("Shutdown connection %p", *it);
+      pjsip_transport_shutdown(*it);
+    }
+  }
+
+  pthread_mutex_unlock(&_lock);
+
+  // If quiescing is now complete notify the quiescing manager.  This is done
+  // without holding the lock to avoid potential deadlocks.
+  if (quiesce_complete) {
+    // TODO Callback into the quiescing manager.
+  }
+}
+
+
+void ConnectionTracker::unquiesce()
+{
+  LOG_DEBUG("Unquiesce connections");
+
+  pthread_mutex_lock(&_lock);
+
+  // It is not possible to "un-shutdown" a pjsip transport.  All connections
+  // that were previously active will eventually be closed. Instead we just
+  // clear the quiescing flag so we won't shut down any new connections that are
+  // established.
+  //
+  // Note it is illegal to call this method if we're not quiescing.
+  assert(_quiescing);
+  _quiescing = PJ_FALSE;
+
+  pthread_mutex_unlock(&_lock);
+}
