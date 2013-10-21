b42c6e0f7341855d3b62c76e71b625d7ad460ae5
Mike Evans <mike@metaswitch.com>
2013-10-21 21:31:05 +0100

    Re-enable geo-redundancy with new memcached redundancy scheme

--- sprout/main.cpp
+++ sprout/main.cpp
@@ -161,12 +161,13 @@ static void usage(void)
        "                            the pre-configured list of IP addresses\n"
        " -R, --realm <realm>        Use specified realm for authentication\n"
        "                            (if not specified, local host name is used)\n"
-       " -M, --memstore <servers>   Use memcached store on comma-separated list of\n"
-       "                            servers for registration state\n"
+       " -M, --memstore <config_file>"
+       "                            Enables local memcached store for registration state and\n"
+       "                            specifies configuration file\n"
        "                            (otherwise uses local store)\n"
-       " -m, --remote-memstore <servers>\n"
-       "                            Use remote memcached store on comma-separated list of\n"
-       "                            servers for registration state\n"
+       " -m, --remote-memstore <config file>\n"
+       "                            Enabled remote memcached store for geo-redundant storage\n"
+       "                            of registration state, and specifies configuration file\n"
        "                            (otherwise uses no remote memcached store)\n"
        " -S, --sas <ipv4>           Use specified host as software assurance\n"
        "                            server.  Otherwise uses localhost\n"
@@ -325,12 +326,12 @@ static pj_status_t init_options(int argc, char *argv[], struct options *options)
 
     case 'M':
       options->store_servers = std::string(pj_optarg);
-      fprintf(stdout, "Using memcached store on servers %s\n", pj_optarg);
+      fprintf(stdout, "Using memcached store with configuration file %s\n", pj_optarg);
       break;
 
     case 'm':
       options->remote_store_servers = std::string(pj_optarg);
-      fprintf(stdout, "Using remote memcached store on servers %s\n", pj_optarg);
+      fprintf(stdout, "Using remote memcached store with configuration file %s\n", pj_optarg);
       break;
 
     case 'S':
@@ -926,7 +927,7 @@ int main(int argc, char *argv[])
   delete enum_service;
   delete bgcf_service;
   delete quiescing_mgr;
-  delete load_monitor; 
+  delete load_monitor;
 
   if (opt.store_servers != "")
   {
