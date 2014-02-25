a2a5c59c3b22381d332938a912d189a519c24450
Andy Caldwell <andrew.caldwell@metaswitch.com>
2014-02-25 17:53:58 +0000

    Add header name

--- include/constants.h
+++ include/constants.h
@@ -75,6 +75,7 @@ const pj_str_t STR_AUTS = pj_str("auts");
 const pj_str_t STR_CK = pj_str("ck");
 const pj_str_t STR_IK = pj_str("ik");
 const pj_str_t STR_P_PROFILE_KEY = pj_str("P-Profile-Key");
+const pj_str_t STR_SESSION_EXPIRES = pj_str("Session-Expires");
 
 /// Prefix of ODI tokens we generate.
 const pj_str_t STR_ODI_PREFIX = pj_str("odi_");
