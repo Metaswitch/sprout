eef9c8d0fa98c41792891543d08501d6eca7482f
Eleanor Merry <eleanor.merry@metaswitch.com>
2014-05-14 15:52:31 +0000

    Check for route headers in OPTIONS

--- sprout/options.cpp
+++ sprout/options.cpp
@@ -72,10 +72,11 @@ pj_bool_t on_rx_request(pjsip_rx_data* rdata)
 {
   if (rdata->msg_info.msg->line.req.method.id == PJSIP_OPTIONS_METHOD)
   {
-    if (PJUtils::is_uri_local(rdata->msg_info.msg->line.req.uri))
+    if (PJUtils::is_uri_local(rdata->msg_info.msg->line.req.uri) &&
+        rdata->msg_info.route == NULL )
     {
-      // OPTIONS targetted at this node or at the home domain, so respond
-      // statelessly.
+      // OPTIONS targetted at this node/home domain with no route
+      // headers. Respond statelessly.
       PJUtils::respond_stateless(stack_data.endpt, rdata, 200, NULL, NULL, NULL);
       return PJ_TRUE;
     }
