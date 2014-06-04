1cdaa4d1d9ac5bf70d2d124b4332f3cb191b37a3
Alex Hockey <alex.hockey@metaswitch.com>
2014-06-04 17:00:54 +0100

    Add UT repro

--- sprout/ut/icscfproxy_test.cpp
+++ sprout/ut/icscfproxy_test.cpp
@@ -2389,3 +2389,43 @@ TEST_F(ICSCFProxyTest, RequestErrors)
 }
 
 
+TEST_F(ICSCFProxyTest, RouteOrigInviteBadServerName)
+{
+  pjsip_tx_data* tdata;
+
+  // Create a TCP connection to the I-CSCF listening port.
+  TransportFlow* tp = new TransportFlow(TransportFlow::Protocol::TCP,
+                                        stack_data.icscf_port,
+                                        "1.2.3.4",
+                                        49152);
+
+  // Set up the HSS response for the originating location query.
+  _hss_connection->set_result("/impu/sip%3A6505551000%40homedomain/location?originating=true",
+                              "{\"result-code\": 2001,"
+                              " \"scscf\": \"BANG!\"}");
+
+  // Inject a INVITE request with orig in the Route header and a P-Served-User
+  // header.
+  Message msg1;
+  msg1._method = "INVITE";
+  msg1._via = tp->to_string(false);
+  msg1._extra = "Contact: sip:6505551000@" +
+                tp->to_string(true) +
+                ";ob;expires=300;+sip.ice;reg-id=1;+sip.instance=\"<urn:uuid:00000000-0000-0000-0000-b665231f1213>\"\r\n";
+  msg1._extra += "P-Served-User: <sip:6505551000@homedomain>";
+  msg1._route = "Route: <sip:homedomain;orig>";
+  inject_msg(msg1.get_request(), tp);
+
+  // Expect 484 Address Incomplete response.
+  ASSERT_EQ(1, txdata_count());
+
+  // Check the response.
+  tdata = current_txdata();
+  RespMatcher(484).matches(tdata->msg);
+  tp->expect_target(tdata);
+  free_txdata();
+
+  _hss_connection->delete_result("/impu/sip%3A6505551000%40homedomain/location?originating=true");
+
+  delete tp;
+}
