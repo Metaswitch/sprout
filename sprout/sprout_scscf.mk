f65e6652d72c50cf2f8cd986d72fd0c7b8e3f51f
Mike Evans <mike@metaswitch.com>
2014-09-29 14:48:53 +0000

    sprout-base plus sproutlet plugin make files

---
+++ sprout/sprout_scscf.mk
@@ -0,0 +1,43 @@
+# sprout-scscf Makefile
+
+all: stage-build
+
+ROOT := $(abspath $(shell pwd)/../)
+MK_DIR := ${ROOT}/mk
+
+TARGET := sprout_scscf.so
+
+TARGET_SOURCES := scscfsproutlet.cpp \
+                  scscfplugin.cpp
+
+CPPFLAGS += -Wno-write-strings \
+            -ggdb3 -std=c++0x
+#	Build location independent code for shared object
+CPPFLAGS += -fpic
+CPPFLAGS += -I${ROOT}/include \
+            -I${ROOT}/modules/cpp-common/include \
+            -I${ROOT}/modules/app-servers/include \
+            -I${ROOT}/usr/include \
+            -I${ROOT}/modules/rapidjson/include
+
+CPPFLAGS += $(shell PKG_CONFIG_PATH=${ROOT}/usr/lib/pkgconfig pkg-config --cflags libpjproject)
+
+# Production build:
+#
+# Enable optimization in production only.
+CPPFLAGS := $(filter-out -O2,$(CPPFLAGS))
+CPPFLAGS_BUILD += -O2
+
+LDFLAGS += -L${ROOT}/usr/lib -rdynamic -shared
+
+include ${MK_DIR}/platform.mk
+
+.PHONY: stage-build
+stage-build: build
+
+.PHONY: debug
+debug: | build_test
+	gdb --args $(TARGET_BIN_TEST) $(EXTRA_TEST_ARGS)
+
+.PHONY: distclean
+distclean: clean
