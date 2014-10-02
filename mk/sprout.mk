89d1a7ed9b4703ba5fcb5d74e9f1d763bad718e6
Mike Evans <mike@metaswitch.com>
2014-10-02 08:43:53 +0000

    Make file structural changes

--- mk/sprout.mk
+++ mk/sprout.mk
@@ -12,21 +12,12 @@ SPROUT_DIR := ${ROOT}/sprout
 SPROUT_TEST_DIR := ${ROOT}/tests
 
 sprout: pjsip libmemcached
-	make -f sprout_base.mk -C ${SPROUT_DIR}
-	make -f sprout_scscf.mk -C ${SPROUT_DIR}
-	make -f sprout_icscf.mk -C ${SPROUT_DIR}
-	make -f sprout_bgcf.mk -C ${SPROUT_DIR}
-	make -f sprout_mmtel_as.mk -C ${SPROUT_DIR}
+	make -C ${SPROUT_DIR}
 
 sprout_test:
 	make -C ${SPROUT_DIR} test
 
 sprout_clean:
-	make -f sprout_base.mk -C ${SPROUT_DIR} clean
-	make -f sprout_scscf.mk -C ${SPROUT_DIR} clean
-	make -f sprout_icscf.mk -C ${SPROUT_DIR} clean
-	make -f sprout_bgcf.mk -C ${SPROUT_DIR} clean
-	make -f sprout_mmtel_as.mk -C ${SPROUT_DIR} clean
 	make -C ${SPROUT_DIR} clean
 	-make -C ${SPROUT_TEST_DIR} clean
 
