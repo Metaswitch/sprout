82bfd1a1612f2748d48c64f623901b19c266a060
Rob Day <rkd@rkd.me.uk>
2014-10-05 12:12:27 +0000

    Fix up icid-value parsing when it's just an IPv6 address

--- sprout/custom_headers.cpp
+++ sprout/custom_headers.cpp
@@ -589,7 +589,17 @@ pjsip_hdr* parse_hdr_p_charging_vector(pjsip_parse_ctx* ctx)
 
   // Parse the required icid-value parameter first.
   pjsip_parse_param_imp(scanner, pool, &name, &value,
-                        PJSIP_PARSE_REMOVE_QUOTE);
+                        NULL);
+
+  // Strip the quotes of manually instead of using
+  // PJ_PARSE_REMOVE_QUOTE. This preserves the square bracket on IPv6
+  // addresses.
+  if (value.ptr[0] == '"' && (value.slen > 2))
+  {
+    value.ptr++;
+    value.slen -= 2;
+  }
+
 
   if (!pj_stricmp2(&name, "icid-value")) {
     hdr->icid = value;
