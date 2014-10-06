90db99493ba8906b2ebd27cc49ffe14d197cd41f
Eleanor Merry <eleanor.merry@metaswitch.com>
2014-10-06 13:13:00 +0000

    Update memento as build

--- mk/memento.mk
+++ mk/memento.mk
@@ -3,13 +3,7 @@
 MEMENTO_DIR := ${MODULE_DIR}/memento
 
 memento:
-	cd modules/memento && git submodule update --init && make -C ${MEMENTO_DIR} object
-	cp ${MEMENTO_DIR}/*.a ${LIB_DIR}/
-	cp ${MEMENTO_DIR}/usr/lib/libthrift* ${LIB_DIR}/
-	cp ${MEMENTO_DIR}/usr/lib/libcassandra* ${LIB_DIR}/
-	cp ${MEMENTO_DIR}/include/*.h ${INCLUDE_DIR}
-	cp ${MEMENTO_DIR}/usr/include/*ass* ${INCLUDE_DIR}
-	cp -R ${MEMENTO_DIR}/usr/include/thrift* ${INCLUDE_DIR}
+	cd modules/memento && git submodule update --init && make -C ${MEMENTO_DIR}
 
 memento_test:
 	cd modules/memento && make -C ${MEMENTO_DIR} test
--- sprout/memento_as.mk
+++ sprout/memento_as.mk
@@ -8,20 +8,29 @@ MK_DIR := ${ROOT}/mk
 TARGET := memento-as.so
 
 TARGET_SOURCES := sproutletappserver.cpp \
-                  mementoasplugin.cpp
+                  mementoasplugin.cpp \
+                  call_list_store.cpp \
+                  call_list_xml.cpp \
+                  mementosaslogger.cpp \
+                  call_list_store_processor.cpp \
+                  mementoappserver.cpp
 
 CPPFLAGS += -Wno-write-strings \
             -ggdb3 -std=c++0x
 
-#	Build location independent code for shared object
+# Build location independent code for shared object
 CPPFLAGS += -fpic
 CPPFLAGS += -I${ROOT}/include \
+            -I${ROOT}/modules/memento/include \
             -I${ROOT}/modules/cpp-common/include \
             -I${ROOT}/modules/app-servers/include \
             -I${ROOT}/usr/include \
             -I${ROOT}/modules/rapidjson/include
 
 CPPFLAGS += $(shell PKG_CONFIG_PATH=${ROOT}/usr/lib/pkgconfig pkg-config --cflags libpjproject)
+
+# Add memento/src as VPATH so build will find modules there.
+VPATH = ${ROOT}/modules/memento/src
 
 # Production build:
 #
@@ -30,8 +39,7 @@ CPPFLAGS := $(filter-out -O2,$(CPPFLAGS))
 CPPFLAGS_BUILD += -O2
 
 LDFLAGS += -L${ROOT}/usr/lib -shared
-LDFLAGS += -lmemento \
-           -lthrift \
+LDFLAGS += -lthrift \
            -lcassandra
 
 include ${MK_DIR}/platform.mk
