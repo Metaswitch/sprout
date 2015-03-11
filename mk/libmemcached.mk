aa99b63d1a06f5c3946370ba08a7dedb46eaa53d
Rob Day <Robert.Day@metaswitch.com>
2015-03-11 20:04:32 +0000

    Remove 'configure' from libmemcached on a 'make clean', so that autoconf gets re-run on the next 'make'

--- mk/libmemcached.mk
+++ mk/libmemcached.mk
@@ -22,9 +22,10 @@ libmemcached_test: libevhtp ${LIBMEM_MAKEFILE}
 
 libmemcached_clean: ${LIBMEM_MAKEFILE}
 	${MAKE} -C ${LIBMEM_DIR} clean
+	rm ${LIBMEM_CONFIGURE}
 
 libmemcached_distclean: ${LIBMEM_MAKEFILE}
 	${MAKE} -C ${LIBMEM_DIR} distclean
-
+	rm ${LIBMEM_CONFIGURE}
 
 .PHONY: libmemcached libmemcached_test libmemcached_clean libmemcached_distclean
