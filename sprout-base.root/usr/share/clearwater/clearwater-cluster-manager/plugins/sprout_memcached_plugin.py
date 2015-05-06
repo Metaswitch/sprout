bb91db6671ffbc4d13a4cc306022087ee5e8d6a6
Rob Day <Robert.Day@metaswitch.com>
2015-05-06 10:25:13 +0000

    Memcached plugin changes

--- sprout-base.root/usr/share/clearwater/clearwater-cluster-manager/plugins/sprout_memcached_plugin.py
+++ sprout-base.root/usr/share/clearwater/clearwater-cluster-manager/plugins/sprout_memcached_plugin.py
@@ -1,7 +1,8 @@
+
 from metaswitch.clearwater.cluster_manager.plugin_base import \
     SynchroniserPluginBase
 from metaswitch.clearwater.cluster_manager.plugin_utils import \
-    send_sighup, write_cluster_settings
+    run_command, write_cluster_settings
 from metaswitch.clearwater.cluster_manager.alarms import issue_alarm
 from metaswitch.clearwater.cluster_manager import constants
 import logging
@@ -11,8 +12,7 @@ _log = logging.getLogger("memcached_plugin")
 
 
 class SproutMemcachedPlugin(SynchroniserPluginBase):
-    def __init__(self):
-        _log.debug("Raising not-clustered alarm")
+    def __init__(self, _ip):
         issue_alarm(constants.RAISE_MEMCACHED_NOT_YET_CLUSTERED)
 
     def key(self):
@@ -21,23 +21,18 @@ class SproutMemcachedPlugin(SynchroniserPluginBase):
     def on_cluster_changing(self, cluster_view):
         write_cluster_settings("/etc/clearwater/cluster_settings",
                                cluster_view)
-        _log.debug("Restarting Sprout")
-        send_sighup("/var/run/sprout.pid")
+        run_command("service sprout reload")
 
     def on_joining_cluster(self, cluster_view):
-        _log.debug("Sprout Memcached cluster is changing")
         self.on_cluster_changing(cluster_view)
 
     def on_new_cluster_config_ready(self, cluster_view):
-        _log.debug("Started running Astaire")
-        os.system("service astaire reload")
-        os.system("service astaire wait-sync")
-        _log.debug("Finished running Astaire")
+        run_command("service astaire reload")
+        run_command("service astaire wait-sync")
 
     def on_stable_cluster(self, cluster_view):
         self.on_cluster_changing(cluster_view)
         issue_alarm(constants.CLEAR_MEMCACHED_NOT_YET_CLUSTERED)
-        _log.debug("Sprout Memcached cluster is stable again")
 
     def on_leaving_cluster(self, cluster_view):
         pass
