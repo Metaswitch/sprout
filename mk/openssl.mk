11e2c10804c6bae6277fbb89fdb9383e90c1b876
Doug Theobald <doug.theobald@metaswitch.com>
2015-06-04 07:02:25 -0700

    Fixes for Ubuntu 14 gcc toolchain (CC_ubu14fix-73)

--- mk/openssl.mk
+++ mk/openssl.mk
@@ -12,7 +12,10 @@ ${OPENSSL_CONFIG_MARKER}:
 
 openssl: ${OPENSSL_CONFIG_MARKER}
 	${MAKE} -C ${OPENSSL_DIR}
-	${MAKE} -C ${OPENSSL_DIR} install
+	# We can't make 'install' because it makes target 'install_docs'
+	# which doesn't build properly using Ubuntu 14.04 tools. As such,
+	# we'll just make it and only install the software.
+	${MAKE} -C ${OPENSSL_DIR} install_sw
 
 openssl_test: ${OPENSSL_CONFIG_MARKER}
 	${MAKE} -C ${OPENSSL_DIR} test
