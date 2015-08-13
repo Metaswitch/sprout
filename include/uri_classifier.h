89e2e2491ae94f82c80987dfa080c9e9475f3bc2
Rob Day <Robert.Day@metaswitch.com>
2015-08-13 10:11:21 +0000

    Check in missing files

---
+++ include/uri_classifier.h
@@ -0,0 +1,67 @@
+/**
+ * Project Clearwater - IMS in the Cloud
+ * Copyright (C) 2015  Metaswitch Networks Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version, along with the "Special Exception" for use of
+ * the program along with SSL, set forth below. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * The author can be reached by email at clearwater@metaswitch.com or by
+ * post at Metaswitch Networks Ltd, 100 Church St, Enfield EN2 6BQ, UK
+ *
+ * Special Exception
+ * Metaswitch Networks Ltd  grants you permission to copy, modify,
+ * propagate, and distribute a work formed by combining OpenSSL with The
+ * Software, or a work derivative of such a combination, even if such
+ * copying, modification, propagation, or distribution would otherwise
+ * violate the terms of the GPL. You must comply with the GPL in all
+ * respects for all of the code used other than OpenSSL.
+ * "OpenSSL" means OpenSSL toolkit software distributed by the OpenSSL
+ * Project and licensed under the OpenSSL Licenses, or a work based on such
+ * software and licensed under the OpenSSL Licenses.
+ * "OpenSSL Licenses" means the OpenSSL License and Original SSLeay License
+ * under which the OpenSSL Project distributes the OpenSSL toolkit software,
+ * as those licenses appear in the file LICENSE-OPENSSL.
+ */
+
+#ifndef URI_CLASSIFIER_H
+#define URI_CLASSIFIER_H
+
+extern "C" {
+#include <pjsip.h>
+#include <pjlib-util.h>
+#include <pjlib.h>
+}
+
+enum URIClass
+{
+  UNKNOWN = 0,
+  LOCAL_PHONE_NUMBER,
+  GLOBAL_PHONE_NUMBER,
+  NODE_LOCAL_SIP_URI,
+  HOME_DOMAIN_SIP_URI,
+  OFFNET_SIP_URI,
+  NP_DATA,
+  FINAL_NP_DATA
+};
+
+namespace URIClassifier
+{
+  URIClass classify_uri(pjsip_uri* uri, bool prefer_sip = true);
+
+  bool is_user_numeric(pj_str_t user);
+
+  extern bool enforce_user_phone;
+  extern bool enforce_global;
+  extern std::vector<pj_str_t*> home_domains;
+};
+
+#endif
---
+++ sprout/uri_classifier.cpp
@@ -0,0 +1,178 @@
+/**
+ * Project Clearwater - IMS in the Cloud
+ * Copyright (C) 2015  Metaswitch Networks Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version, along with the "Special Exception" for use of
+ * the program along with SSL, set forth below. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * The author can be reached by email at clearwater@metaswitch.com or by
+ * post at Metaswitch Networks Ltd, 100 Church St, Enfield EN2 6BQ, UK
+ *
+ * Special Exception
+ * Metaswitch Networks Ltd  grants you permission to copy, modify,
+ * propagate, and distribute a work formed by combining OpenSSL with The
+ * Software, or a work derivative of such a combination, even if such
+ * copying, modification, propagation, or distribution would otherwise
+ * violate the terms of the GPL. You must comply with the GPL in all
+ * respects for all of the code used other than OpenSSL.
+ * "OpenSSL" means OpenSSL toolkit software distributed by the OpenSSL
+ * Project and licensed under the OpenSSL Licenses, or a work based on such
+ * software and licensed under the OpenSSL Licenses.
+ * "OpenSSL Licenses" means the OpenSSL License and Original SSLeay License
+ * under which the OpenSSL Project distributes the OpenSSL toolkit software,
+ * as those licenses appear in the file LICENSE-OPENSSL.
+ */
+
+#include <vector>
+#include "uri_classifier.h"
+#include "stack.h"
+#include "constants.h"
+
+std::vector<pj_str_t*> URIClassifier::home_domains;
+bool URIClassifier::enforce_global;
+bool URIClassifier::enforce_user_phone;
+
+bool URIClassifier::is_user_numeric(pj_str_t user)
+{
+  const char* uri = user.ptr;
+
+  for (int i = 0; i < user.slen; i++)
+  {
+    if ((uri[i] == '+') ||
+        (uri[i] == '-') ||
+        (uri[i] == '.') ||
+        (uri[i] == '(') ||
+        (uri[i] == ')') ||
+        (uri[i] == '[') ||
+        (uri[i] == ']') ||
+        ((uri[i] >= '0') && 
+         (uri[i] <= '9')))
+    {
+      continue;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+static bool is_home_domain(pj_str_t host)
+{
+    for (unsigned int i = 0; i < URIClassifier::home_domains.size(); ++i)
+    {
+      if (pj_stricmp(&host, URIClassifier::home_domains[i]) == 0)
+      {
+        return true;
+      }
+    }
+
+  /* Doesn't match */
+  return false;
+}
+
+static bool is_local_name(pj_str_t host)
+{
+    for (unsigned int i = 0; i < stack_data.name_cnt; ++i)
+    {
+      if (pj_stricmp(&host, &stack_data.name[i]) == 0)
+      {
+        return true;
+      }
+    }
+
+  /* Doesn't match */
+  return false;
+}
+
+
+URIClass URIClassifier::classify_uri(pjsip_uri* uri, bool prefer_sip)
+{
+  URIClass ret = URIClass::UNKNOWN;
+  bool has_rn = false;
+  bool has_npdi = false;
+
+  if (PJSIP_URI_SCHEME_IS_TEL(uri))
+  {
+    // If the URI is a tel URI, pull out the information from the other_params
+    has_rn = (pjsip_param_find(&((pjsip_tel_uri*)uri)->other_param, &STR_RN) != NULL);
+    has_npdi = (pjsip_param_find(&((pjsip_tel_uri*)uri)->other_param, &STR_NPDI) != NULL);
+  }
+  else if (PJSIP_URI_SCHEME_IS_SIP(uri))
+  {
+    // If the URI is a tel URI, pull out the information from the userinfo_params
+    has_rn = (pjsip_param_find(&((pjsip_sip_uri*)uri)->userinfo_param, &STR_RN) != NULL);
+    has_npdi = (pjsip_param_find(&((pjsip_sip_uri*)uri)->userinfo_param, &STR_NPDI) != NULL);
+  }
+
+  if (has_rn)
+  {
+    if (has_npdi)
+    {
+      ret = FINAL_NP_DATA;
+    }
+    else
+    {
+      ret = NP_DATA;
+    }
+  }
+  else if (PJSIP_URI_SCHEME_IS_TEL(uri))
+  {
+    pjsip_tel_uri* tel_uri = (pjsip_tel_uri*)uri;
+    if (tel_uri->number.slen > 0 && tel_uri->number.ptr[0] == '+')
+    {
+      ret = GLOBAL_PHONE_NUMBER;
+    }
+    else
+    {
+      ret = enforce_global ? LOCAL_PHONE_NUMBER : GLOBAL_PHONE_NUMBER;
+    }
+  }
+  else if (PJSIP_URI_SCHEME_IS_SIP(uri))
+  {
+    pjsip_sip_uri* sip_uri = (pjsip_sip_uri*)uri;
+    pj_str_t host = sip_uri->host;
+    bool home_domain = is_home_domain(host);
+    bool local_to_node = is_local_name(host);
+    bool is_gruu = (pjsip_param_find(&((pjsip_sip_uri*)uri)->other_param, &STR_GR) != NULL);
+    bool treat_number_as_phone = !enforce_user_phone && !prefer_sip;
+
+    if ((!pj_strcmp(&((pjsip_sip_uri*)uri)->user_param, &STR_USER_PHONE) ||
+         (home_domain && treat_number_as_phone && !is_gruu)))
+    {
+      if (sip_uri->user.slen > 0 && sip_uri->user.ptr[0] == '+')
+      {
+        ret = GLOBAL_PHONE_NUMBER;
+      }
+      else
+      {
+        ret = enforce_global ? LOCAL_PHONE_NUMBER : GLOBAL_PHONE_NUMBER;
+      }
+    }
+    else if (home_domain)
+    {
+      ret = HOME_DOMAIN_SIP_URI;
+    }
+    else if (local_to_node)
+    {
+      ret = NODE_LOCAL_SIP_URI;
+    }
+    else
+    {
+      ret = OFFNET_SIP_URI;
+    }
+  }
+
+  TRC_DEBUG("Classified URI as %d", (int)ret);
+  return ret;
+}
