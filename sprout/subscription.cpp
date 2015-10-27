a29709e6069f1dfe37c8d419790524c19bf8790f
Adam Lindley <adam.lindley@metaswitch.com>
2015-10-27 17:43:19 +0000

    sub module should ask reg store to set timers

--- sprout/subscription.cpp
+++ sprout/subscription.cpp
@@ -270,7 +270,7 @@ pj_status_t write_subscriptions_to_store(RegStore* primary_store,      ///<store
     }
 
     // Try to write the AoR back to the store.
-    set_rc = primary_store->set_aor_data(aor, (*aor_data), false, trail, false);
+    set_rc = primary_store->set_aor_data(aor, (*aor_data), true, trail, false);
 
     if (set_rc != Store::OK)
     {
