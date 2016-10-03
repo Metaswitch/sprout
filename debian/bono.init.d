57d6f3b431e842caa1b20e5f45c20358f8124ad0
George Belsten <George.Belsten@metaswitch.com>
2016-10-03 14:52:41 +0100

    Changed bono alias to domain, to allow for routing to ibcf by domain

--- debian/bono.init.d
+++ debian/bono.init.d
@@ -154,7 +154,7 @@ get_daemon_args()
 
         DAEMON_ARGS="--domain=$home_domain
                      --localhost=$local_ip,$public_hostname
-                     --alias=$public_ip
+                     --alias=ibcf.$home_domain
                      --pcscf=5060,5058
                      --webrtc-port=5062
                      --routing-proxy=$upstream_hostname,$upstream_port,$upstream_connections,$upstream_recycle_connections
