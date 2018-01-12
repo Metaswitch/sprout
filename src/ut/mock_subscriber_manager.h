44eac7889f2e7a73d4839a5d2d32188d2e58348c
Ying Huang <Ying.Huang@metaswitch.com>
2018-01-12 18:09:27 +0000

    use right API

--- src/ut/mock_subscriber_manager.h
+++ src/ut/mock_subscriber_manager.h
@@ -21,27 +21,52 @@ public:
   MockSubscriberManager();
   virtual ~MockSubscriberManager();
 
-  MOCK_METHOD3(get_bindings, HTTPCode(std::string public_id,
-                                      std::vector<Binding>& bindings,
-                                      SAS::TrailId trail));
-
-  MOCK_METHOD3(update_associated_uris, HTTPCode(std::string aor_id,
-                                                AssociatedURIs associated_uris,
-                                                SAS::TrailId trail));
+  MOCK_METHOD6(update_bindings, HTTPCode(HSSConnection::irs_query irs_query,
+                                         const std::vector<Binding>& updated_bindings,
+                                         std::vector<std::string> binding_ids_to_remove,
+                                         std::vector<Binding>& all_bindings,
+                                         HSSConnection::irs_info& irs_info,
+                                         SAS::TrailId trail));
 
   MOCK_METHOD4(remove_bindings, HTTPCode(std::vector<std::string> binding_ids,
                                          EventTrigger event_trigger,
                                          std::vector<Binding>& bindings,
                                          SAS::TrailId trail));
 
-  MOCK_METHOD3(get_cached_subscriber_state, HTTPCode(std::string public_id,
-                                                     SubscriberInfo& subscriber_info,
-                                                     SAS::TrailId trail));
+  MOCK_METHOD4(update_subscription, HTTPCode(std::string public_id,
+                                             const Subscription& subscription,
+                                             HSSConnection::irs_info& irs_info,
+                                             SAS::TrailId trail));
+
+  MOCK_METHOD4(remove_subscription, HTTPCode(std::string public_id,
+                                             std::string subscription_id,
+                                             HSSConnection::irs_info& irs_info,
+                                             SAS::TrailId trail));
+
+  MOCK_METHOD3(deregister_subscriber, HTTPCode(std::string public_id,
+                                               EventTrigger event_trigger,
+                                               SAS::TrailId trail));
+
+  MOCK_METHOD3(get_bindings, HTTPCode(std::string public_id,
+                                      std::vector<Binding>& bindings,
+                                      SAS::TrailId trail));
 
   MOCK_METHOD4(get_bindings_and_subscriptions, HTTPCode(std::string public_id,
                                                         std::vector<Binding>& bindings,
                                                         std::vector<Subscription>& subscriptions,
                                                         SAS::TrailId trail));
+
+  MOCK_METHOD3(get_cached_subscriber_state, HTTPCode(std::string public_id,
+                                                     HSSConnection::irs_info& irs_info,
+                                                     SAS::TrailId trail));
+
+  MOCK_METHOD3(get_subscriber_state, HTTPCode(HSSConnection::irs_query irs_query,
+                                              HSSConnection::irs_info& irs_info,
+                                              SAS::TrailId trail));
+
+  MOCK_METHOD3(update_associated_uris, HTTPCode(std::string aor_id,
+                                                AssociatedURIs associated_uris,
+                                                SAS::TrailId trail));
 };
 
 #endif
