b1bc270a78fb4ae95ad22545311496045748047f
Ken Ferguson <ken.ferguson@metaswitch.com>
2018-01-16 17:40:36 +0000

    Set the PJSIP Ram Recorder

--- src/stack.cpp
+++ src/stack.cpp
@@ -249,6 +249,7 @@ void init_pjsip_logging(int log_level,
   pj_log_set_level(log_level);
   pj_log_set_decor(PJ_LOG_HAS_SENDER);
   pj_log_set_log_func(&pjsip_log_handler);
+  pj_log_set_ram_trace_funcs(&Log::ramCacheTrcCall, &Log::ramTrace);
 }
 
 
