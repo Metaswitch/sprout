4c27a05f47d84f30bff947f48c5eba7e8ca204d5
Ying Huang <Ying.Huang@metaswitch.com>
2018-01-23 19:24:07 +0000

    new files

---
+++ src/ut/s4_handler_test.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file handlers.cpp
+ *
+ * Copyright (C) Metaswitch Networks 2018
+ * If license terms are provided to you in a COPYING file in the root directory
+ * of the source code repository by which you are accessing this code, then
+ * the license outlined in that COPYING file applies to your use.
+ * Otherwise no rights are granted except for those provided to you by
+ * Metaswitch Networks in a separate written agreement.
+ */
